d2f45bec6df9c06594d677ea91945e85aa6197b3
Talha <talhadar2011@gmail.com>
2025-09-29 15:50:33 +0200

    customHooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
-
+import { usePointerPosition } from './pointerPosition';
+import { useDelayedValue } from './dely';
 import './App.css'
   function VideoPlayer({ src, isPlaying }) {
   const ref = useRef(null);
@@ -29,10 +30,35 @@ function App() {
   const [isPlaying, setIsPlaying] = useState(false);
 
 
-
+function Dot({ position, opacity }) {
+  return (
+    <div style={{
+      position: 'absolute',
+      backgroundColor: 'pink',
+      borderRadius: '50%',
+      opacity,
+      transform: `translate(${position.x}px, ${position.y}px)`,
+      pointerEvents: 'none',
+      left: -20,
+      top: -20,
+      width: 40,
+      height: 40,
+    }} />
+  );
+}
+const pos1 = usePointerPosition();
+  const pos2 = useDelayedValue(pos1, 100);
+  const pos3 = useDelayedValue(pos2, 200);
+  const pos4 = useDelayedValue(pos3, 100);
+  const pos5 = useDelayedValue(pos4, 50);
 
   return (
     <>
+    <Dot position={pos1} opacity={1} />
+      <Dot position={pos2} opacity={0.8} />
+      <Dot position={pos3} opacity={0.6} />
+      <Dot position={pos4} opacity={0.4} />
+      <Dot position={pos5} opacity={0.2} />
       <button onClick={handleClick}>
       Click me!
     </button>
